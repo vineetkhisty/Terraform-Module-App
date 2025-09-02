48d0f343caf02edf41a62b81e01ece88ea5b354d
VineetKhisty <vineetk2008@gmail.com>
2025-09-02 15:35:12 +0530

    Added EKS Module

---
+++ terraform-eks/vpc.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${local.name}-vpc"
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = local.env
+  }
+  
+}
